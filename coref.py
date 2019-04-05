scoring.py; loss-calculation bracket-bug

coref.py
@@ -2,117 +2,23 @@
 # Early stopping
 import io
 
-import attr
-
-from loader import Span, GLOVE, lookup_tensor, test_corpus, val_corpus, train_corpus
-from utils import to_cuda, compute_idx_spans, unpack_and_unpad, pack, flatten, extract_gold_corefs, safe_divide, \
-    pad_and_stack, to_var, pairwise_indexes, speaker_label, prune
+from loader import GLOVE, lookup_tensor, test_corpus, val_corpus, train_corpus
+from scoring import PairwiseScore, MentionScorer
+from utils import to_cuda, unpack_and_unpad, pack, flatten, extract_gold_corefs, safe_divide
 
 print('Initializing...')
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torchtext.vocab import Vectors
 
 import random
 import numpy as np
 import networkx as nx
 from tqdm import tqdm
-from random import sample
 from datetime import datetime
 from subprocess import Popen, PIPE
-from boltons.iterutils import pairwise
 import os
-
-class Score(nn.Module):
-    """ Generic scoring module
-    """
-    def __init__(self, embeds_dim, hidden_dim=150):
-        super().__init__()
-
-        self.score = nn.Sequential(
-            nn.Linear(embeds_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Dropout(0.20),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Dropout(0.20),
-            nn.Linear(hidden_dim, 1)
-        )
-
-    def forward(self, x):
-        """ Output a scalar score for an input x """
-        return self.score(x)
-
-
-class Distance(nn.Module):
-    """ Learned, continuous representations for: span widths, distance
-    between spans
-    """
-
-    bins = [1,2,3,4,8,16,32,64]
-
-    def __init__(self, distance_dim=20):
-        super().__init__()
-
-        self.dim = distance_dim
-        self.embeds = nn.Sequential(
-            nn.Embedding(len(self.bins)+1, distance_dim),
-            nn.Dropout(0.20)
-        )
-
-    def forward(self, *args):
-        """ Embedding table lookup """
-        return self.embeds(self.stoi(*args))
-
-    def stoi(self, lengths):
-        """ Find which bin a number falls into """
-        return to_cuda(torch.tensor([
-            sum([True for i in self.bins if num >= i]) for num in lengths], requires_grad=False
-        ))
-
-
-class Genre(nn.Module):
-    """ Learned continuous representations for genre. Zeros if genre unknown.
-    """
-
-    genres = ['bc', 'bn', 'mz', 'nw', 'pt', 'tc', 'wb']
-    _stoi = {genre: idx+1 for idx, genre in enumerate(genres)}
-
-    def __init__(self, genre_dim=20):
-        super().__init__()
-
-        self.embeds = nn.Sequential(
-            nn.Embedding(len(self.genres)+1, genre_dim, padding_idx=0),
-            nn.Dropout(0.20)
-        )
-
-    def forward(self, labels):
-        """ Embedding table lookup """
-        return self.embeds(self.stoi(labels))
-
-    def stoi(self, labels):
-        """ Locate embedding id for genre """
-        indexes = [self._stoi.get(gen) for gen in labels]
-        return to_cuda(torch.tensor([i if i is not None else 0 for i in indexes]))
-
-
-class Speaker(nn.Module):
-    """ Learned continuous representations for binary speaker. Zeros if speaker unknown.
-    """
-
-    def __init__(self, speaker_dim=20):
-        super().__init__()
-
-        self.embeds = nn.Sequential(
-            nn.Embedding(3, speaker_dim, padding_idx=0),
-            nn.Dropout(0.20)
-        )
-
-    def forward(self, speaker_labels):
-        """ Embedding table lookup (see src.utils.speaker_label fnc) """
-        return self.embeds(to_cuda(torch.tensor(speaker_labels)))
 
 
 class CharCNN(nn.Module):
@@ -241,158 +147,6 @@ class DocumentEncoder(nn.Module):
         return embeds
 
 
-class MentionScore(nn.Module):
-    """ Mention scoring module
-    """
-    def __init__(self, gi_dim, attn_dim, distance_dim):
-        super().__init__()
-
-        self.attention = Score(attn_dim)
-        self.width = Distance(distance_dim)
-        self.score = Score(gi_dim)
-
-    def forward(self, states, embeds, doc, K=250):
-        """ Compute unary mention score for each span
-        """
-
-        # Initialize Span objects containing start index, end index, genre, speaker
-        spans = [Span(i1=i[0], i2=i[-1], id=idx,
-                      speaker=doc.speaker(i), genre=doc.genre)
-                 for idx, i in enumerate(compute_idx_spans(doc.sents))]
-
-        # Compute first part of attention over span states (alpha_t)
-        attns = self.attention(states)
-
-        # Regroup attn values, embeds into span representations
-        # TODO: figure out a way to batch
-        span_attns, span_embeds = zip(*[(attns[s.i1:s.i2+1], embeds[s.i1:s.i2+1])
-                                        for s in spans])
-
-        # Pad and stack span attention values, span embeddings for batching
-        padded_attns, _ = pad_and_stack(span_attns, value=-1e10)
-        padded_embeds, _ = pad_and_stack(span_embeds)
-
-        # Weight attention values using softmax
-        attn_weights = F.softmax(padded_attns, dim=1)
-
-        # Compute self-attention over embeddings (x_hat)
-        attn_embeds = torch.sum(torch.mul(padded_embeds, attn_weights), dim=1)
-
-        # Compute span widths (i.e. lengths), embed them
-        widths = self.width([len(s) for s in spans])
-
-        # Get LSTM state for start, end indexes
-        # TODO: figure out a way to batch
-        start_end = torch.stack([torch.cat((states[s.i1], states[s.i2]))
-                                 for s in spans])
-
-        # Cat it all together to get g_i, our span representation
-        g_i = torch.cat((start_end, attn_embeds, widths), dim=1)
-
-        # Compute each span's unary mention score
-        mention_scores = self.score(g_i)
-
-        # Update span object attributes
-        # (use detach so we don't get crazy gradients by splitting the tensors)
-        spans = [
-            attr.evolve(span, si=si)
-            for span, si in zip(spans, mention_scores.detach())
-        ]
-
-        # Prune down to LAMBDA*len(doc) spans
-        spans = prune(spans, len(doc))
-
-        # Update antencedent set (yi) for each mention up to K previous antecedents
-        spans = [
-            attr.evolve(span, yi=spans[max(0, idx-K):idx])
-            for idx, span in enumerate(spans)
-        ]
-
-        return spans, g_i, mention_scores
-
-
-class PairwiseScore(nn.Module):
-    """ Coreference pair scoring module
-    """
-    def __init__(self, gij_dim, distance_dim, genre_dim, speaker_dim):
-        super().__init__()
-
-        self.distance = Distance(distance_dim)
-        self.genre = Genre(genre_dim)
-        self.speaker = Speaker(speaker_dim)
-
-        self.score = Score(gij_dim)
-
-    def forward(self, spans, g_i, mention_scores):
-        """ Compute pairwise score for spans and their up to K antecedents
-        """
-
-        # Extract raw features
-        mention_ids, antecedent_ids, \
-            distances, genres, speakers = zip(*[(i.id, j.id,
-                                                i.i2-j.i1, i.genre,
-                                                speaker_label(i, j))
-                                             for i in spans
-                                             for j in i.yi])
-
-        # For indexing a tensor efficiently
-        mention_ids = to_cuda(torch.tensor(mention_ids))
-        antecedent_ids = to_cuda(torch.tensor(antecedent_ids))
-
-        # Embed them
-        phi = torch.cat((self.distance(distances),
-                         self.genre(genres),
-                         self.speaker(speakers)), dim=1)
-
-        # Extract their span representations from the g_i matrix
-        i_g = torch.index_select(g_i, 0, mention_ids)
-        j_g = torch.index_select(g_i, 0, antecedent_ids)
-
-        # Create s_ij representations
-        pairs = torch.cat((i_g, j_g, i_g*j_g, phi), dim=1)
-
-        # Extract mention score for each mention and its antecedents
-        s_i = torch.index_select(mention_scores, 0, mention_ids)
-        s_j = torch.index_select(mention_scores, 0, antecedent_ids)
-
-        # Score pairs of spans for coreference link
-        s_ij = self.score(pairs)
-
-        # Compute pairwise scores for coreference links between each mention and
-        # its antecedents
-        coref_scores = torch.sum(torch.cat((s_i, s_j, s_ij), dim=1), dim=1, keepdim=True)
-
-        # Update spans with set of possible antecedents' indices, scores
-        spans = [
-            attr.evolve(span,
-                        yi_idx=[((y.i1, y.i2), (span.i1, span.i2)) for y in span.yi]
-                        )
-            for span, score, (i1, i2) in zip(spans, coref_scores, pairwise_indexes(spans))
-        ]
-
-        # Get antecedent indexes for each span
-        antecedent_idx = [len(s.yi) for s in spans if len(s.yi)]
-
-        # Split coref scores so each list entry are scores for its antecedents, only.
-        # (NOTE that first index is a special case for torch.split, so we handle it here)
-        split_scores = [to_cuda(torch.tensor([]))] \
-                         + list(torch.split(coref_scores, antecedent_idx, dim=0))
-
-        epsilon = to_var(torch.tensor([[0.]]))
-        with_epsilon = [torch.cat((score, epsilon), dim=0) for score in split_scores]
-
-        # Batch and softmax
-        # get the softmax of the scores for each span in the document given
-        probs = [F.softmax(tensr) for tensr in with_epsilon]
-        
-        # pad the scores for each one with a dummy value, 1000 so that the tensors can 
-        # be of the same dimension for calculation loss and what not. 
-        probs, _ = pad_and_stack(probs, value=1000)
-        probs = probs.squeeze()
-       
-        return spans, probs
-
-
 class CorefScore(nn.Module):
     """ Super class to compute coreference links between spans
     """
@@ -416,7 +170,7 @@ class CorefScore(nn.Module):
 
         # Initialize modules
         self.encoder = DocumentEncoder(hidden_dim, char_filters)
-        self.score_spans = MentionScore(gi_dim, attn_dim, distance_dim)
+        self.score_spans = MentionScorer(gi_dim, attn_dim, distance_dim)
         self.score_pairs = PairwiseScore(gij_dim, distance_dim, genre_dim, speaker_dim)
 
     def forward(self, doc):
@@ -554,7 +308,7 @@ class Trainer:
 
         # Negative marginal log-likelihood
         eps = 1e-8
-        loss = torch.sum(torch.log(torch.sum(torch.mul(probs, gold_indexes), dim=1).clamp_(eps, 1-eps), dim=0) * -1)
+        loss = torch.sum(torch.log(torch.sum(torch.mul(probs, gold_indexes), dim=1).clamp_(eps, 1-eps)), dim=0) * -1
 
         # Backpropagate
         loss.backward()
@@ -691,7 +445,7 @@ class Trainer:
 
 
 # Initialize model, train
-model = CorefScore(embeds_dim=400, hidden_dim=200)
+model = CorefScore(embeds_dim=350, hidden_dim=200)
 # ?? train for 150 epochs, each each train 100 documents
 trainer = Trainer(model, train_corpus, val_corpus, test_corpus, steps=100)
 trainer.train(150)

utils.py
@@ -1,11 +1,10 @@
 import torch
 import torch.nn.functional as F
-from torch.autograd import Variable
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pack_sequence
+from torch.nn.utils.rnn import pad_packed_sequence, pack_sequence
 
 import numpy as np
 from boltons.iterutils import pairwise, windowed
-from itertools import groupby, combinations
+from itertools import combinations
 from collections import defaultdict
 
 
@@ -77,23 +76,6 @@ def pack(tensors):
 
     return packed, reorder
 
-
-def prune(spans, T, LAMBDA=0.40):
-    """ Prune mention scores to the top lambda percent.
-    Returns list of tuple(scores, indices, g_i) """
-
-    # Only take top λT spans, where T = len(doc)
-    STOP = int(LAMBDA * T)
-
-    # Sort by mention score, remove overlapping spans, prune to top λT spans
-    sorted_spans = sorted(spans, key=lambda s: s.si, reverse=True)
-    nonoverlapping = remove_overlapping(sorted_spans)
-    pruned_spans = nonoverlapping[:STOP]
-
-    # Resort by start, end indexes
-    spans = sorted(pruned_spans, key=lambda s: (s.i1, s.i2))
-
-    return spans
 
 def remove_overlapping(sorted_spans):
     """ Remove spans that are overlapping by order of decreasing mention score